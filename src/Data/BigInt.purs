d295cb6584da5b31445ffdfeab371330abaad0dd
Michael Trotter <spicydonuts@gmail.com>
2018-05-16 17:13:03 -0600

    Update types to capture possible BigInt creation errors

--- src/Data/BigInt.purs
+++ src/Data/BigInt.purs
@@ -27,9 +27,11 @@ module Data.BigInt
 
 import Prelude
 
+import Control.Monad.Eff.Exception (Error)
 import Data.Array.NonEmpty (NonEmptyArray)
+import Data.Either (Either(..), fromRight)
 import Data.Int (floor)
-import Data.Maybe (Maybe(..), fromJust)
+import Data.Maybe (fromJust)
 import Data.String.NonEmpty (NonEmptyString)
 import Data.String.NonEmpty as NES
 import Partial.Unsafe (unsafePartial)
@@ -37,23 +39,34 @@ import Partial.Unsafe (unsafePartial)
 -- | An arbitrary length integer.
 foreign import data BigInt :: Type
 
-type BaseDigits = {
-    value :: NonEmptyArray Int
+type BaseDigits =
+  { value :: NonEmptyArray Int
   , isNegative :: Boolean
-}
+  }
 
 -- | FFI wrapper to parse a String in a given base representation into a BigInt.
-foreign import fromBase' :: forall a. (a -> Maybe a)
-                         -> Maybe a
-                         -> Int
-                         -> String
-                         -> Maybe BigInt
+foreign import fromBase'
+  :: forall a
+   . (a -> Either Error a)
+  -> (Error -> Either Error a)
+  -> Int
+  -> String
+  -> Either Error BigInt
+
+-- | Truncate a Number to an Int (towards zero).
+-- | `Infinity`, `-Infinity`, and `NaN` pass through.
+foreign import numberToString :: Number -> String
+
+-- | Convert an Int to a String
+foreign import intToString :: Int -> String
 
 -- | Convert an integer to a BigInt.
-foreign import fromInt :: Int -> BigInt
+fromInt :: Int -> Either Error BigInt
+fromInt = fromBase' Right Left 10 <<< intToString
 
 -- | Convert a Number to a BigInt. The fractional part is truncated.
-foreign import fromNumber :: Number -> BigInt
+fromNumber :: Number -> Either Error BigInt
+fromNumber = fromBase' Right Left 10 <<< numberToString
 
 -- | Converts a BigInt to a Number. Loses precision for numbers which are too
 -- | large.
@@ -94,7 +107,7 @@ foreign import shl :: BigInt -> Number -> BigInt
 foreign import shr :: BigInt -> Number -> BigInt
 
 -- | Parse a string into a `BigInt`, assuming a decimal representation. Returns
--- | `Nothing` if the parse fails.
+-- | `Left Error` if the parse fails.
 -- |
 -- | Examples:
 -- | ```purescript
@@ -102,19 +115,19 @@ foreign import shr :: BigInt -> Number -> BigInt
 -- | fromString "857981209301293808359384092830482"
 -- | fromString "1e100"
 -- | ```
-fromString :: String -> Maybe BigInt
+fromString :: String -> Either Error BigInt
 fromString = fromBase 10
 
 -- | Parse a string into a `BigInt`, assuming a representation in the given base.
 -- | The letters "a-z" and "A-Z" will be interpreted as the digits `10` to
--- | `36`. Returns `Nothing` if the parse fails.
+-- | `36`. Returns `Left Error` if the parse fails.
 -- |
 -- | ```purescript
 -- | fromBase 2 "100" == fromString "4"
 -- | fromBase 16 "ff" == fromString "255"
 -- | ```
-fromBase :: Int -> String -> Maybe BigInt
-fromBase = fromBase' Just Nothing
+fromBase :: Int -> String -> Either Error BigInt
+fromBase = fromBase' Right Left
 
 foreign import biEquals :: BigInt -> BigInt -> Boolean
 
@@ -155,9 +168,9 @@ foreign import biMul :: BigInt -> BigInt -> BigInt
 
 instance semiringBigInt :: Semiring BigInt where
   add  = biAdd
-  zero = fromInt 0
+  zero = unsafePartial (fromRight (fromInt 0))
   mul  = biMul
-  one  = fromInt 1
+  one  = unsafePartial (fromRight (fromInt 1))
 
 foreign import biSub :: BigInt -> BigInt -> BigInt
 
--- test/Main.purs
+++ test/Main.purs
@@ -4,17 +4,19 @@ import Prelude hiding (not)
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Exception (EXCEPTION, Error, message)
 import Control.Monad.Eff.Random (RANDOM)
 import Data.Array (filter, range)
 import Data.Array.NonEmpty as NEA
 import Data.BigInt (BigInt, abs, and, digitsInBase, even, fromBase, fromInt, fromNumber, fromString, not, odd, or, pow, prime, shl, shr, toBase, toBase', toNonEmptyString, toNumber, toString, xor)
+import Data.Either (Either(..), either, fromLeft, fromRight, isLeft)
 import Data.Foldable (fold)
 import Data.Int as Int
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Maybe (Maybe(..))
 import Data.NonEmpty ((:|))
 import Data.String.NonEmpty (unsafeFromString)
 import Data.String.NonEmpty as NES
+import Global (infinity, nan)
 import Partial.Unsafe (unsafePartial)
 import Test.Assert (ASSERT, assert)
 import Test.QuickCheck (QC, quickCheck)
@@ -22,7 +24,6 @@ import Test.QuickCheck.Arbitrary (class Arbitrary)
 import Test.QuickCheck.Gen (Gen, arrayOf, chooseInt, elements, resize)
 import Test.QuickCheck.Laws.Data as Data
 import Type.Proxy (Proxy(..))
-
 
 -- | Newtype with an Arbitrary instance that generates only small integers
 newtype SmallInt = SmallInt Int
@@ -44,7 +45,7 @@ derive newtype instance euclideanRingTestBigInt :: EuclideanRing TestBigInt
 
 instance arbitraryBigInt :: Arbitrary TestBigInt where
   arbitrary = do
-    n <- (fromMaybe zero <<< fromString) <$> digitString
+    n <- (either (const zero) id <<< fromString) <$> digitString
     op <- elements (id :| [negate])
     pure (TestBigInt (op n))
     where digits :: Gen Int
@@ -53,66 +54,70 @@ instance arbitraryBigInt :: Arbitrary TestBigInt where
           digitString = (fold <<< map show) <$> (resize 50 $ arrayOf digits)
 
 -- | Convert SmallInt to BigInt
-fromSmallInt :: SmallInt -> BigInt
+fromSmallInt :: SmallInt -> Either Error BigInt
 fromSmallInt = fromInt <<< runSmallInt
 
 -- | Test if a binary relation holds before and after converting to BigInt.
 testBinary :: forall eff. (BigInt -> BigInt -> BigInt)
            -> (Int -> Int -> Int)
            -> QC eff Unit
-testBinary f g = quickCheck (\x y -> (fromInt x) `f` (fromInt y) == fromInt (x `g` y))
+testBinary f g =
+  quickCheck \x y ->
+    case f <$> fromInt x <*> fromInt y, fromInt (x `g` y) of
+      Right fResult, Right gResult -> fResult == gResult
+      _            , _             -> false
 
 main :: forall eff. Eff (console :: CONSOLE, assert :: ASSERT, random :: RANDOM, exception :: EXCEPTION | eff) Unit
-main = do
+main = unsafePartial do
   log "Simple arithmetic operations and conversions from Int"
   let two = one + one
   let three = two + one
   let four = three + one
-  assert $ fromInt 3 == three
+  assert $ fromRight (fromInt 3) == three
   assert $ two * two == four
-  assert $ two * three * (three + four) == fromInt 42
-  assert $ two - three == fromInt (-1)
+  assert $ two * three * (three + four) == fromRight (fromInt 42)
+  assert $ two - three == fromRight (fromInt (-1))
 
   log "Parsing strings"
-  assert $ fromString "2" == Just two
-  assert $ fromString "a" == Nothing
-  assert $ fromString "2.1" == Nothing
-  assert $ fromString "123456789" == Just (fromInt 123456789)
-  assert $ fromString "1e7" == Just (fromInt 10000000)
-  quickCheck $ \(TestBigInt a) -> (fromString <<< toString) a == Just a
+  assert $ fromRight (fromString "2") == two
+  assert $ isLeft $ fromString "a"
+  assert $ isLeft $ fromString "2.1"
+  assert $ fromRight (fromString "123456789") == fromRight (fromInt 123456789)
+  assert $ fromRight (fromString "1e7") == fromRight (fromInt 10000000)
+  quickCheck $ \(TestBigInt a) -> (fromRight <<< fromString <<< toString) a == a
 
   log "Parsing strings with a different base"
-  assert $ fromBase 2 "100" == Just four
-  assert $ fromBase 16 "ff" == fromString "255"
+  assert $ fromRight (fromBase 2 "100") == four
+  assert $ fromRight (fromBase 16 "ff") == fromRight (fromString "255")
 
   log "Rendering bigints as strings with a different base"
   assert $ toBase 2 four == "100"
-  assert $ (toBase 16 <$> fromString "255") == Just "ff"
-  assert $ toString (fromInt 12345) == "12345"
+  assert $ fromRight (toBase 16 <$> fromString "255") == "ff"
+  assert $ toString (fromRight (fromInt 12345)) == "12345"
 
   log "Converting bigints to arrays with a different base"
   assert $ NEA.toArray (digitsInBase 2 four).value == [1, 0, 0]
-  assert $ (NEA.toArray <<< _.value <<< digitsInBase 16 <$>
-           fromString "255") == Just [15, 15]
-  assert $ NEA.toArray (digitsInBase 10 $ fromInt 12345).value
+  assert $ fromRight (NEA.toArray <<< _.value <<< digitsInBase 16 <$>
+           fromString "255") == [15, 15]
+  assert $ NEA.toArray (digitsInBase 10 $ fromRight (fromInt 12345)).value
            == [1, 2, 3, 4, 5]
 
   assert $ toBase' 2 four == unsafePartial unsafeFromString "100"
-  assert $ (toBase' 16 <$> fromString "255") == NES.fromString "ff"
-  assert $ toNonEmptyString (fromInt 12345)
+  assert $ Just (fromRight (toBase' 16 <$> fromString "255")) == NES.fromString "ff"
+  assert $ toNonEmptyString (fromRight (fromInt 12345))
            == unsafePartial unsafeFromString "12345"
 
   log "Converting from Number to BigInt"
-  assert $ fromNumber 0.0 == zero
-  assert $ fromNumber 3.4 == three
-  assert $ fromNumber (-3.9) == -three
-  assert $ fromNumber 1.0e7 == fromInt 10000000
-  assert $ Just (fromNumber 1.0e47) == fromString "1e47"
-  quickCheck (\x -> fromInt x == fromNumber (Int.toNumber x))
+  assert $ fromRight (fromNumber 0.0) == zero
+  assert $ fromRight (fromNumber 3.4) == three
+  assert $ fromRight (fromNumber (-3.9)) == -three
+  assert $ fromRight (fromNumber 1.0e7) == fromRight (fromInt 10000000)
+  assert $ fromRight (fromNumber 1.0e47) == fromRight (fromString "1e47")
+  quickCheck (\x -> fromRight (fromInt x) == fromRight (fromNumber (Int.toNumber x)))
 
   log "Conversions between String, Int and BigInt should not loose precision"
-  quickCheck (\n -> fromString (show n) == Just (fromInt n))
-  quickCheck (\n -> Int.toNumber n == toNumber (fromInt n))
+  quickCheck (\n -> fromRight (fromString (show n)) == fromRight (fromInt n))
+  quickCheck (\n -> Int.toNumber n == toNumber (fromRight (fromInt n)))
 
   log "Binary relations between integers should hold before and after converting to BigInt"
   testBinary (+) (+)
@@ -122,25 +127,25 @@ main = do
   testBinary div div
 
   -- To test the multiplication, we need to make sure that Int does not overflow
-  quickCheck (\x y -> fromSmallInt x * fromSmallInt y == fromInt (runSmallInt x * runSmallInt y))
+  quickCheck (\x y -> fromRight (fromSmallInt x) * fromRight (fromSmallInt y) == fromRight (fromInt (runSmallInt x * runSmallInt y)))
 
   log "It should perform multiplications which would lead to imprecise results using Number"
-  assert $ Just (fromInt 333190782 * fromInt 1103515245) == fromString "367681107430471590"
+  assert $ fromRight (fromInt 333190782 * fromInt 1103515245) == fromRight (fromString "367681107430471590")
 
   log "compare, (==), even, odd should be the same before and after converting to BigInt"
-  quickCheck (\x y -> compare x y == compare (fromInt x) (fromInt y))
-  quickCheck (\x y -> (fromSmallInt x == fromSmallInt y) == (runSmallInt x == runSmallInt y))
-  quickCheck (\x -> Int.even x == even (fromInt x))
-  quickCheck (\x -> Int.odd x == odd (fromInt x))
+  quickCheck (\x y -> compare x y == compare (fromRight (fromInt x)) (fromRight (fromInt y)))
+  quickCheck (\x y -> (fromRight (fromSmallInt x) == fromRight (fromSmallInt y)) == (runSmallInt x == runSmallInt y))
+  quickCheck (\x -> Int.even x == even (fromRight (fromInt x)))
+  quickCheck (\x -> Int.odd x == odd (fromRight (fromInt x)))
 
   log "pow should perform integer exponentiation and yield 0 for negative exponents"
-  assert $ three `pow` four == fromInt 81
+  assert $ three `pow` four == fromRight (fromInt 81)
   assert $ three `pow` -two == zero
   assert $ three `pow` zero == one
   assert $ zero `pow` zero == one
 
   log "Prime numbers"
-  assert $ filter (prime <<< fromInt) (range 2 20) == [2, 3, 5, 7, 11, 13, 17, 19]
+  assert $ filter (prime <<< fromRight <<< fromInt) (range 2 20) == [2, 3, 5, 7, 11, 13, 17, 19]
 
   log "Absolute value"
   quickCheck $ \(TestBigInt x) -> abs x == if x > zero then x else (-x)
@@ -160,5 +165,10 @@ main = do
   Data.checkOrd prxBigInt
   Data.checkSemiring prxBigInt
   Data.checkRing prxBigInt
---  Data.checkEuclideanRing prxBigInt
+  -- Data.checkEuclideanRing prxBigInt
   Data.checkCommutativeRing prxBigInt
+
+  log "Infinity and NaN"
+  assert $ message (fromLeft (fromNumber infinity)) == "Invalid integer: Infinity"
+  assert $ message (fromLeft (fromNumber (-infinity))) == "Invalid integer: Infinity"
+  assert $ message (fromLeft (fromNumber nan)) == "Invalid integer: NaN"
